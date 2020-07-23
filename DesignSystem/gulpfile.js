934d85393b3b6f8075f6540799ff303f864c93b1
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-07-23 11:12:45 -0500

    Clase 8

--- DesignSystem/gulpfile.js
+++ DesignSystem/gulpfile.js
@@ -1,13 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
+//npm install gulp --save
 gulp.task('hello', done=>{
   console.log('Hello world!')
   done()
 });
-
+//npm install gulp-sass
 gulp.task('sass', function(){
   return gulp.src('scss/**/*.scss')
     .pipe(sass()) //Convierte SASS a CSS CON GULP-SASS
     .pipe(gulp.dest('public/stylesheets'))
+});
+//npm install gulp-minify-css
+gulp.task("style_min", () => {
+  return gulp
+    .src("scss/**/*.scss")
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(concat("style_main.min.css"))
+    .pipe(gulp.dest("public/stylesheets"));
+});
+
+gulp.task("watch", () => {
+  gulp.watch("scss/**/*.scss", gulp.series("style_min"));
 });
